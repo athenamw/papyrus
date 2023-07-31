57904654971feb77b2fd8c2d61b27b7f3b9ac8a6
athenamw <wallisathena@gmail.com>
2023-07-30 18:03:49 -0700

    updated server.js and added id on db.json

--- routes/notes.js
+++ routes/notes.js
@@ -1,46 +1,62 @@
-const router = require('express').Router();
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-// GET one user
-router.get('/:id', async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.params.id);
-    if (!userData) {
-      res.status(404).json({ message: 'No user with this id!' });
-      return;
-    }
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST create a new user
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// PUT update a user
-router.put('/:id', async (req, res) => {
-  try {
-    const userData = await User.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-      individualHooks: true,
+// GET Route for a specific note
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteId);
+      if (result.length > 0) {
+        return res.json(result);
+      } else {
+        return res.json('No note with that ID');
+      }
     });
-    if (!userData[0]) {
-      res.status(404).json({ message: 'No user with this id!' });
-      return;
-    }
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
+});
+
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted.`);
+    });
+});
+
+// POST Route for a new UX/UI note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newnote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newnote, './db/db.json');
+    res.json(`note added successfully!`);
+  } else {
+    res.error('Error adding note');
   }
 });
 
-module.exports = router;
+module.exports = notes;
