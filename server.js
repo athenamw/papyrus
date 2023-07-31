465ac63a52ada7f0782fd5eec8dfda2ba3407bdf
athenamw <wallisathena@gmail.com>
2023-07-30 18:19:46 -0700

    removed 404 file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 // GET Route for notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/404.html')));
+// gets index.html page for wildcards
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
